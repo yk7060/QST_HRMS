5548f2376feea5a055c28cbb9ad5eb65c4bad2dc
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-01 23:36:28 +0530

    Update EmployeeBenefitController.java

--- Backend/src/main/java/com/quantumsoft/hrms/controller/EmployeeBenefitController.java
+++ Backend/src/main/java/com/quantumsoft/hrms/controller/EmployeeBenefitController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin("*")
+// @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/employee-benefits")
 @RequiredArgsConstructor
