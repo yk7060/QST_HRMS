3364a07feaf4850edcc64eccda74b9460e257d46
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-01 23:37:58 +0530

    Update OptionalHolidayController.java

--- Backend/src/main/java/com/quantumsoft/hrms/controller/OptionalHolidayController.java
+++ Backend/src/main/java/com/quantumsoft/hrms/controller/OptionalHolidayController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
+// @CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/api/optionalHoliday")
 public class OptionalHolidayController {
