b059ff221e7caf0bc2e7ac5ef8c002e135f9a25c
Dadarao Raut <115204873+DadaraoRaut@users.noreply.github.com>
2025-07-11 15:00:32 +0530

    Update environment.ts

--- Frontend/src/components/environment.ts
+++ Frontend/src/components/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://13.221.21.162:8080/api',
+  apiBaseUrl: 'http://34.227.160.147:8080/api',
   appName: 'HRMS',
   enableDebug: false
 };
