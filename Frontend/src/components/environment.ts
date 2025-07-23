12cc7ba7282f158cff5867c014f4cef109b78e87
yk7060 <ykathar132@gmail.com>
2025-07-23 10:46:52 +0530

    environment.ts

--- Frontend/src/components/environment.ts
+++ Frontend/src/components/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://13.51.235.79:8080/api',
+  apiBaseUrl: 'http://13.49.41.193:8080/api',
   appName: 'HRMS',
   enableDebug: false
 };
